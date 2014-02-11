395963c5f25f7a1b9fa94353cb5e465a12393303
Bernhard Häussner <bxt@die-optimisten.net>
2014-02-11 05:54:38 +0100

    Fix smooth orthogonal drawer by taking ports into account on L/G edges

--- src/de/uniwue/smooth/draw/SmoothOrthogonalDrawer.java
+++ src/de/uniwue/smooth/draw/SmoothOrthogonalDrawer.java
@@ -1,5 +1,6 @@
 package de.uniwue.smooth.draw;
 
+import de.uniwue.smooth.app.EdgeTestDrawings;
 import de.uniwue.smooth.orthogonal.OrthogonalLayout;
 import de.uniwue.smooth.orthogonal.Port;
 import de.uniwue.smooth.util.Util;
@@ -57,18 +58,30 @@ public class SmoothOrthogonalDrawer<V, E, T> extends AbstractOrthogonalDrawer<V,
 				
 			} else { // L or G
 				
+				Quadrant lQuadrant = null;
+				Port fIDS1 = null;
+				Port fIDS2 = null;
+				int mask = 0;
+				if(ports.getFirst() == Port.R && ports.getSecond() == Port.T) { lQuadrant = Quadrant.IV ; fIDS1 = Port.B; fIDS2 = Port.L; mask = 0b11001; }
+				if(ports.getFirst() == Port.B && ports.getSecond() == Port.L) { lQuadrant = Quadrant.IV ; fIDS1 = Port.T; fIDS2 = Port.R; mask = 0b01100; }
+				if(ports.getFirst() == Port.T && ports.getSecond() == Port.L) { lQuadrant = Quadrant.I  ; fIDS1 = Port.R; fIDS2 = Port.B; mask = 0b10010; }
+				if(ports.getFirst() == Port.R && ports.getSecond() == Port.B) { lQuadrant = Quadrant.I  ; fIDS1 = Port.L; fIDS2 = Port.T; mask = 0b00111; }
+				if(ports.getFirst() == Port.L && ports.getSecond() == Port.B) { lQuadrant = Quadrant.II ; fIDS1 = Port.B; fIDS2 = Port.L; mask = 0b01001; }
+				if(ports.getFirst() == Port.T && ports.getSecond() == Port.R) { lQuadrant = Quadrant.II ; fIDS1 = Port.T; fIDS2 = Port.R; mask = 0b11100; }
+				if(ports.getFirst() == Port.B && ports.getSecond() == Port.R) { lQuadrant = Quadrant.III; fIDS1 = Port.R; fIDS2 = Port.B; mask = 0b00010; }
+				if(ports.getFirst() == Port.L && ports.getSecond() == Port.T) { lQuadrant = Quadrant.III; fIDS1 = Port.L; fIDS2 = Port.T; mask = 0b10111; }
+				
 				int dx =  vertexCoordinates.getSecond().getFirst() - vertexCoordinates.getFirst().getFirst();
 				int dy =  vertexCoordinates.getSecond().getSecond() - vertexCoordinates.getFirst().getSecond();
-				boolean slopeGreaterOne = Math.abs(dx / dy) > 1;
-				boolean firstIsVertical = ports.getFirst().isVertical();
-				boolean slopePositive = dx > 0 == dy > 0;
-				boolean lTyped = firstIsVertical ? ports.getFirst().equals(Port.T) == dy > 0 : ports.getFirst().equals(Port.R) == dx > 0;
+				Quadrant quadrant = quadrant(dx, dy);
+				boolean upper = (0 < (mask & 8) ? -1 : 1) * dx < dy;
+				boolean lTyped = quadrant == lQuadrant;
 				
-				boolean firstIsDiagonalStart = firstIsVertical == slopeGreaterOne == lTyped; // choose second or first point as reference for further calculations
-				boolean mid_changeXandAddDy = slopeGreaterOne == lTyped; // which delta value to add to which coordinate
-				boolean mid_subtract = slopePositive != firstIsDiagonalStart; // if or not to subtract the delta value from the reference point
+				boolean firstIsDiagonalStart = upper != (0 < (mask & 16)); // choose second or first point as reference for further calculations
+				boolean mid_changeXandAddDy = firstIsDiagonalStart != (0 < (mask & 1)); // which delta value to add to which coordinate
+				boolean mid_subtract = firstIsDiagonalStart != (0 < (mask & 2)); // if or not to subtract the delta value from the reference point
 				boolean kink_subtract = !firstIsDiagonalStart; // if or not to subtract the delta value from the midpoint
-				boolean diagStartFirst = slopePositive != slopeGreaterOne; // if or not to go ccw from ref to kink or the other way around
+				boolean diagStartFirst = firstIsDiagonalStart != lTyped != (0 < (mask & 4)); // if or not to go ccw from ref to kink or the other way around
 				
 				Pair<Integer> mid; // center of the circle arc
 				Pair<Integer> kink; // point connecting line and circle arc
@@ -98,4 +111,15 @@ public class SmoothOrthogonalDrawer<V, E, T> extends AbstractOrthogonalDrawer<V,
 		return Util.getKeyByValue(layout.getPortAssignment(v), e);
 	}
 	
+	private static Quadrant quadrant(int x, int y) {
+		if(x>0) {
+			return y > 0 ? Quadrant.I : Quadrant.IV;
+		} else {
+			return y > 0 ? Quadrant.II : Quadrant.III;
+		}
+	}
+	
+	private enum Quadrant {
+		I,II,III,IV;	
+	}
 }
