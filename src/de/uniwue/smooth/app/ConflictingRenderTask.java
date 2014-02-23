fb872b6266d931854b9ffb4ddedb4f419e293696
Bernhard Häussner <bxt@die-optimisten.net>
2014-02-23 20:35:09 +0100

    Add base for drawing conflicting graphs

---
+++ src/de/uniwue/smooth/app/ConflictingRenderTask.java
@@ -0,0 +1,106 @@
+package de.uniwue.smooth.app;
+
+import java.awt.geom.AffineTransform;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import de.uniwue.smooth.draw.OrthogonalDrawer;
+import de.uniwue.smooth.draw.StraightlineOrthogonalDrawer;
+import de.uniwue.smooth.draw.OrthogonalDrawing;
+import de.uniwue.smooth.draw.OrthogonalIpeDrawing;
+import de.uniwue.smooth.draw.SmoothOrthogonalDrawer;
+import de.uniwue.smooth.draw.TransformingOrthogonalDrawing;
+import de.uniwue.smooth.orthogonal.CompressingLiuEtAlLayout;
+import de.uniwue.smooth.orthogonal.LiuEtAlLayout;
+import de.uniwue.smooth.orthogonal.OrthogonalLayout;
+import de.uniwue.smooth.palm.ListStOrdering;
+import de.uniwue.smooth.palm.StOrdering;
+import de.uniwue.smooth.util.NotAnExecutorService;
+import de.uniwue.smooth.util.Util;
+import de.uniwue.smooth.util.tuples.ImmutableTuple;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.util.Pair;
+import edu.uci.ics.jung.io.GraphIOException;
+import edu.uci.ics.jung.io.GraphReader;
+
+
+public class ConflictingRenderTask implements Runnable {
+	
+	public static void main(String[] args) throws Exception {
+		List<ConflictingRenderTask> tasks = Arrays.asList
+				( new ConflictingRenderTask(30, 400, 400, "gates")
+				);
+		ExecutorService executor = new NotAnExecutorService(); Executors.newFixedThreadPool(8);
+		for (Runnable task : tasks) executor.execute(task);
+		executor.shutdown();
+	}
+		
+	private static Graph<Vertex, Edge> getGraph(String name) {
+		File file = new File("resources/conflicting_graphs/" + name + ".graphml");
+		GraphReader<Graph<Vertex, Edge>, Vertex, Edge> graphReader = GraphReaderFactory.create(file);
+		Graph<Vertex, Edge> graph;
+		try {
+			graph = graphReader.readGraph();
+		} catch (GraphIOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return graph;
+	}
+	
+	private double scale;
+	private double translateX;
+	private double translateY;
+	private Graph<Vertex, Edge> graph;
+	private String name;
+	private StOrdering<Vertex, Edge> stOrdering = null;
+	
+	public ConflictingRenderTask(double scale, double translateX, double translateY,
+			String name) {
+		this.scale = scale;
+		this.translateX = translateX;
+		this.translateY = translateY;
+		this.name = name;
+		this.graph = getGraph(name);
+	}
+
+	public ConflictingRenderTask(double scale, double translateX, double translateY,
+			String name, int[] preStOrdering) {
+		this(scale, translateX, translateY, name);
+		this.stOrdering = makeStOrdering(preStOrdering, graph);
+	}
+
+	private static StOrdering<Vertex, Edge> makeStOrdering(int[] preStOrdering, Graph<Vertex, Edge> graph) {
+		List<Vertex> vertexInList = new ArrayList<>(graph.getVertices());
+		List<Vertex> vertexOutList = new ArrayList<>();
+		for(int index : preStOrdering) vertexOutList.add(vertexInList.get(index));
+		if(new HashSet<>(vertexOutList).size() != vertexInList.size()) throw new IllegalStateException("Mising vertices in custom st order");
+		return new ListStOrdering<>(vertexOutList);
+	}
+
+	@Override
+	public void run() {
+		Collection<OrthogonalLayout<Vertex, Edge>> layouts = Collections.<OrthogonalLayout<Vertex, Edge>>singleton(new CompressingLiuEtAlLayout<Vertex, Edge>(graph, stOrdering));
+		Pair<OrthogonalDrawer<Vertex, Edge, String>> drawers = new Pair<OrthogonalDrawer<Vertex, Edge, String>>(
+				new StraightlineOrthogonalDrawer<Vertex, Edge, String>(), new SmoothOrthogonalDrawer<Vertex, Edge, String>());
+		for(OrthogonalLayout<Vertex, Edge> layout : layouts) {
+			layout.initialize();
+			AffineTransform transform = new AffineTransform();
+			transform.translate(translateX, translateY);
+			transform.scale(scale, scale);
+			for(OrthogonalDrawer<Vertex, Edge, String> drawer : drawers) {
+				if(layout.getClass() == LiuEtAlLayout.class && drawer.getClass() == SmoothOrthogonalDrawer.class) continue;
+				OrthogonalDrawing<String> drawing = new TransformingOrthogonalDrawing<>(new OrthogonalIpeDrawing(), transform);
+				String ipeCode = drawer.transform(new ImmutableTuple<>(layout, drawing));
+				Util.writeFile("resources/conflicting_graphs/drawings/"+name+"-"+layout.getClass().getSimpleName()+"-"+drawer.getClass().getSimpleName()+".ipe", ipeCode);
+			}
+		}
+	}
+}
