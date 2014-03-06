5cd7a541b311448ce7ff568bb04dd08984e9c166
Bernhard Häussner <bxt@die-optimisten.net>
2014-03-06 13:29:06 +0100

    Add the sector utility with tests

--- src/de/uniwue/smooth/collison/Sector.java
+++ src/de/uniwue/smooth/collison/Sector.java
@@ -2,11 +2,58 @@ package de.uniwue.smooth.collison;
 
 import java.awt.geom.Point2D;
 
+import de.uniwue.smooth.util.point2d.Point2DOperations;
+
 
 /**
  * Represents the space between two angles around the coordinate origin.
  */
 public class Sector {
+	
+	private static final double TWO_PI = 2*Math.PI;
+	
+	/**
+	 * Create a sector between two points, exclusive.
+	 * 
+	 * It is assumed that the x axis points upwards and the sector is between from
+	 * and to in counterclockwise direction.
+	 * 
+	 * @param from Starting point.
+	 * @param to End point.
+	 * @return Sector between these points.
+	 */
+	public static Sector getSectorBetween(Point2D from, Point2D to) {
+		return getSectorBetween(Point2DOperations.atan2(from), Point2DOperations.atan2(to));
+	}
+
+	/**
+	 * Create a sector between two angles.
+	 * 
+	 * @param from Starting angle, exclusive.
+	 * @param to End angle, exclusive.
+	 * @return Sector between the given angles.
+	 */
+	public static Sector getSectorBetween(double from, double to) {
+		return getSectorFrom(from, to - from);
+	}
+	
+	/**
+	 * Create a sector from an angle with a certain extend.
+	 * @param from Starting angle, exclusive.
+	 * @param extend Maximum offset from starting angle, exclusive.
+	 * @return Sector at the given start with the given extend.
+	 */
+	public static Sector getSectorFrom(double from, double extend) {
+		return new Sector(normalizeAngle(from), normalizeAngle(extend));
+	}
+
+	private double from;
+	private double extend;
+	
+	private Sector(double from, double extend) {
+		this.from = from;
+		this.extend = extend;
+	}
 	
 	/**
 	 * Check if a points is in this sector.
@@ -14,8 +61,13 @@ public class Sector {
 	 * @return If or not the point is in this sector.
 	 */
 	public boolean contains(Point2D point) {
-		return false; // TODO
+		double offset = normalizeAngle(Point2DOperations.atan2(point) - from);
+		return offset > 0 && offset < extend;
 	}
-
+	
+	private static double normalizeAngle(double a) {
+		a = a % TWO_PI;
+		return a < 0 ? a + TWO_PI : a;
+	}
 
 }
---
+++ test/de/uniwue/smooth/collison/SectorTest.java
@@ -0,0 +1,126 @@
+package de.uniwue.smooth.collison;
+
+import static org.junit.Assert.*;
+
+import java.awt.geom.Point2D;
+
+import org.junit.Test;
+
+public class SectorTest {
+
+	@Test
+	public void testContainsA() {
+		Sector sector = Sector.getSectorBetween(p(0, 1), p(-1, 0));
+		
+		assertTrue(sector.contains(p(-1,  1)));
+		
+		// distance doesn't matter
+		assertTrue(sector.contains(p(-2,  2)));
+		
+		// borders exclusive
+		assertFalse(sector.contains(p( 0,  1)));
+		assertFalse(sector.contains(p(-1,  0)));
+		
+		assertFalse(sector.contains(p( 1,  1)));
+		assertFalse(sector.contains(p(-1, -1)));
+		assertFalse(sector.contains(p( 1, -1)));
+	}
+
+	@Test
+	public void testContainsB() {
+		Sector sector = Sector.getSectorBetween(p(-1, 0), p(0, 1));
+		
+		assertTrue(sector.contains(p( 1,  1)));
+		assertTrue(sector.contains(p(-1, -1)));
+		assertTrue(sector.contains(p( 1, -1)));
+		
+		// distance doesn't matter
+		assertTrue(sector.contains(p( 2,  2)));
+		
+		// borders exclusive
+		assertFalse(sector.contains(p( 0,  1)));
+		assertFalse(sector.contains(p(-1,  0)));
+		
+		assertFalse(sector.contains(p(-1,  1)));
+		assertFalse(sector.contains(p(-2,  2)));
+	}
+
+	@Test
+	public void testContainsC() {
+		Sector sector = Sector.getSectorBetween(p(1, 1), p(1, -1));
+		
+		assertTrue(sector.contains(p( 0,  1)));
+		assertTrue(sector.contains(p( 0,  2)));
+		assertTrue(sector.contains(p( 0, -1)));
+		assertTrue(sector.contains(p( 0, -2)));
+		assertTrue(sector.contains(p( 1,  2)));
+		assertTrue(sector.contains(p( 1, -2)));
+		
+		assertFalse(sector.contains(p( 1,  0)));
+		assertFalse(sector.contains(p( 2,  0)));
+		assertFalse(sector.contains(p( 2,  1)));
+		assertFalse(sector.contains(p( 2, -1)));
+		
+		assertFalse(sector.contains(p( 1,  1)));
+		assertTrue(sector.contains(p(-1, -1)));
+		assertFalse(sector.contains(p( 1, -1)));		
+		assertTrue(sector.contains(p(-1,  1)));
+	}
+
+	@Test
+	public void testContainsD() {
+		Sector sector = Sector.getSectorBetween(p(1, -1), p(1, 1));
+		
+		assertFalse(sector.contains(p( 0,  1)));
+		assertFalse(sector.contains(p( 0,  2)));
+		assertFalse(sector.contains(p( 0, -1)));
+		assertFalse(sector.contains(p( 0, -2)));
+		assertFalse(sector.contains(p( 1,  2)));
+		assertFalse(sector.contains(p( 1, -2)));
+		
+		assertTrue(sector.contains(p( 1,  0)));
+		assertTrue(sector.contains(p( 2,  0)));
+		assertTrue(sector.contains(p( 2,  1)));
+		assertTrue(sector.contains(p( 2, -1)));
+		
+		assertFalse(sector.contains(p( 1,  1)));
+		assertFalse(sector.contains(p(-1, -1)));
+		assertFalse(sector.contains(p( 1, -1)));
+		assertFalse(sector.contains(p(-1,  1)));
+	}
+
+	@Test
+	public void testContainsE() {
+		Sector sector = Sector.getSectorBetween(p(0, 1), p(0, -1));
+		
+		assertFalse(sector.contains(p( 1,  1)));
+		assertTrue(sector.contains(p(-1, -1)));
+		assertFalse(sector.contains(p( 1, -1)));
+		assertTrue(sector.contains(p(-1,  1)));
+	}
+
+	@Test
+	public void testContainsF() {
+		Sector sector = Sector.getSectorBetween(p(1, 1), p(-1, -1));
+		
+		assertFalse(sector.contains(p( 1,  1)));
+		assertFalse(sector.contains(p(-1, -1)));
+		assertFalse(sector.contains(p( 1, -1)));
+		assertTrue(sector.contains(p(-1,  1)));
+	}
+
+	@Test
+	public void testContainsG() {
+		Sector sector = Sector.getSectorBetween(p(-1, -1), p(1, 1));
+		
+		assertFalse(sector.contains(p( 1,  1)));
+		assertFalse(sector.contains(p(-1, -1)));
+		assertTrue(sector.contains(p( 1, -1)));
+		assertFalse(sector.contains(p(-1,  1)));
+	}
+
+	private static Point2D p(double x, double y) {
+		return new Point2D.Double(x, y);
+	}
+	
+}
