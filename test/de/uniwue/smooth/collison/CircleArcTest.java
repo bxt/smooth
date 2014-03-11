e9f01d627cd24dc6e71c7e5430974e6fc00e279c
Bernhard Häussner <bxt@die-optimisten.net>
2014-03-11 13:41:50 +0100

    Add tests for circle arc - line intersections

--- test/de/uniwue/smooth/collison/CircleArcTest.java
+++ test/de/uniwue/smooth/collison/CircleArcTest.java
@@ -45,15 +45,78 @@ public class CircleArcTest {
 	}
 
 	@Test
-	public void testIntersectionsLine() {
+	public void testIntersectionsLineOne() {
 		CircleArc circleArc = CircleArc.getCircleArc(p(6, 2), p(1, 2), p(1, 7));
 		LineSegment lineSegment = new LineSegment(p(4, 5), p(6, 5));
 		
 		List<Point2D> intersections = sortIntersections(circleArc.intersections(lineSegment));
 		assertEquals(1, intersections.size());
 		
+		assertEquals(5,	intersections.get(0).getX(), DELTA);
+		assertEquals(5,	intersections.get(0).getY(), DELTA);
+	}
+	
+	@Test
+	public void testIntersectionsLineOneCicleBigEnough() {
+		CircleArc circleArc = CircleArc.getCircleArc(p(6, 2), p(1, 2), p(-4, 2));
+		LineSegment lineSegment = new LineSegment(p(4, 5), p(6, 5));
+		
+		List<Point2D> intersections = sortIntersections(circleArc.intersections(lineSegment));
+		assertEquals(1, intersections.size());
+		
+		assertEquals(5,	intersections.get(0).getX(), DELTA);
+		assertEquals(5,	intersections.get(0).getY(), DELTA);
+	}
+	
+	@Test
+	public void testIntersectionsLineOneLineBigEnough() {
+		CircleArc circleArc = CircleArc.getCircleArc(p(6, 2), p(1, 2), p(1, 7));
+		LineSegment lineSegment = new LineSegment(p(-4, 5), p(6, 5));
+		
+		List<Point2D> intersections = sortIntersections(circleArc.intersections(lineSegment));
+		assertEquals(1, intersections.size());
+		
+		assertEquals(5,	intersections.get(0).getX(), DELTA);
+		assertEquals(5,	intersections.get(0).getY(), DELTA);
+	}
+	
+	@Test
+	public void testIntersectionsLineTwoA() {
+		CircleArc circleArc = CircleArc.getCircleArc(p(6, 2), p(1, 2), p(-4, 2));
+		LineSegment lineSegment = new LineSegment(p(-4, 5), p(6, 5));
+		
+		List<Point2D> intersections = sortIntersections(circleArc.intersections(lineSegment));
+		assertEquals(2, intersections.size());
+		
+		assertEquals(-3,	intersections.get(0).getX(), DELTA);
+		assertEquals( 5,	intersections.get(0).getY(), DELTA);
+		
+		assertEquals( 5,	intersections.get(1).getX(), DELTA);
+		assertEquals( 5,	intersections.get(1).getY(), DELTA);
+	}
+	
+	@Test
+	public void testIntersectionsLineTwoB() {
+		CircleArc circleArc = CircleArc.getCircleArc(p(6, 2), p(1, 2), p(1, 7));
+		LineSegment lineSegment = new LineSegment(p(3, 7), p(6, 4));
+		
+		List<Point2D> intersections = sortIntersections(circleArc.intersections(lineSegment));
+		assertEquals(2, intersections.size());
+		
+		assertEquals(4,	intersections.get(0).getX(), DELTA);
+		assertEquals(6,	intersections.get(0).getY(), DELTA);
+		
 		assertEquals(5,	intersections.get(1).getX(), DELTA);
 		assertEquals(5,	intersections.get(1).getY(), DELTA);
+	}
+	
+	@Test
+	public void testNoIntersectionsLine() {
+		CircleArc circleArc = CircleArc.getCircleArc(p(6, 2), p(1, 2), p(1, 7));
+		LineSegment lineSegment = new LineSegment(p(6, -2), p(6, 2));
+		
+		List<Point2D> intersections = sortIntersections(circleArc.intersections(lineSegment));
+		assertEquals(0, intersections.size());
 	}
 	
 }
