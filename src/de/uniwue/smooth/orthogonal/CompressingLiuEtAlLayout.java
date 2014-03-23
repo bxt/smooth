4faa052aa1229d1fcdf88b8b1df868184b7e5359
Bernhard Häussner <bxt@die-optimisten.net>
2014-03-23 20:43:22 +0100

    Add method to retrieve equal height vertex sets from compressing layout

--- src/de/uniwue/smooth/orthogonal/CompressingLiuEtAlLayout.java
+++ src/de/uniwue/smooth/orthogonal/CompressingLiuEtAlLayout.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 import org.apache.commons.collections15.CollectionUtils;
+import org.apache.commons.collections15.Factory;
 import org.apache.commons.collections15.MapUtils;
+import org.apache.commons.collections15.comparators.ComparableComparator;
+import org.apache.commons.collections15.comparators.TransformingComparator;
 import org.apache.commons.collections15.functors.MapTransformer;
 
 import de.uniwue.smooth.palm.StOrdering;
@@ -26,7 +31,9 @@ import edu.uci.ics.jung.graph.Graph;
  * @param <E> Edge type.
  */
 public class CompressingLiuEtAlLayout<V, E> extends LiuEtAlLayout<V, E> {
-		
+	
+	private List<Set<V>> sets;
+	
 	public CompressingLiuEtAlLayout(Graph<V, E> graph,
 			StOrdering<V, E> stOrdering) {
 		super(graph, stOrdering);
@@ -46,6 +53,14 @@ public class CompressingLiuEtAlLayout<V, E> extends LiuEtAlLayout<V, E> {
 	}
 
 	/**
+	 * The list of vertex sets placed at one height, ordered by height.
+	 * @return List of vertex sets placed at one height, ordered by height.
+	 */
+	public List<Set<V>> getSets() {
+		return sets;
+	}
+	
+	/**
 	 * An optional step which moves vertices connected by s-shaped edges
 	 * into the same row and optimizes the height of the drawing.
 	 */
@@ -53,12 +68,16 @@ public class CompressingLiuEtAlLayout<V, E> extends LiuEtAlLayout<V, E> {
 	protected void compression() { // TODO: remove columns of s-edges?
 		DirectedGraph<Integer, E> setGraph = new DirectedSparseGraph<>();
 		final Map<V, Integer> vertexSet = new HashMap<>();
+		final Map<Integer, Set<V>> sets = MapUtils.lazyMap(new HashMap<Integer, Set<V>>(), new Factory<Set<V>>() {
+			@Override public Set<V> create() { return new HashSet<>(); }
+		});
+		
 		
 		int setCounter = 0;
 		for (V v : getGraph().getVertices()) {
 			if(!vertexSet.containsKey(v)) {
-				assignVertexSet(setCounter, vertexSet, v, Port.L);
-				assignVertexSet(setCounter, vertexSet, v, Port.R);
+				assignVertexSet(setCounter, vertexSet, v, Port.L, sets.get(setCounter));
+				assignVertexSet(setCounter, vertexSet, v, Port.R, sets.get(setCounter));
 				setGraph.addVertex(setCounter);
 				setCounter++;
 			}
@@ -94,6 +113,11 @@ public class CompressingLiuEtAlLayout<V, E> extends LiuEtAlLayout<V, E> {
 		for (V v : getGraph().getVertices()) {
 			vertexRows.put(v, setHeights.get(vertexSet.get(v)));
 		}
+		
+		List<Integer> setsSortedByHeight = new ArrayList<Integer>(setHeights.keySet());
+		Collections.sort(setsSortedByHeight, new TransformingComparator<>(MapTransformer.getInstance(setHeights), new ComparableComparator<Integer>()));
+		this.sets = new ArrayList<Set<V>>();
+		CollectionUtils.collect(setsSortedByHeight, MapTransformer.getInstance(sets), this.sets);
 	}
 	
 	/**
@@ -121,7 +145,7 @@ public class CompressingLiuEtAlLayout<V, E> extends LiuEtAlLayout<V, E> {
 	 * @param v Start vertex.
 	 * @param direction Direction to find s-edges at.
 	 */
-	private void assignVertexSet(int setCounter, Map<V, Integer> vertexSet, V v, Port direction) {
+	private void assignVertexSet(int setCounter, Map<V, Integer> vertexSet, V v, Port direction, Set<V> set) {
 		V u, w = v; E e = null;
 		do {
 			if(e != null) {
@@ -129,6 +153,7 @@ public class CompressingLiuEtAlLayout<V, E> extends LiuEtAlLayout<V, E> {
 				edgeColumns.put(e, vertexColumns.get(v));
 			}
 			vertexSet.put(w, setCounter);
+			set.add(w);
 			u = w;
 			e = getPortAssignment(u).get(direction);
 			if(e != null) w = getGraph().getOpposite(u, e);
