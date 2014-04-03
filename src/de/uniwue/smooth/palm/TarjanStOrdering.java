0674d6a5de2ae0fbceb3ecc77ea08f0266ca96d3
Bernhard Häussner <bxt@die-optimisten.net>
2014-04-03 21:41:57 +0200

    Add comments to the Tarjan st-Ordering implementation

--- src/de/uniwue/smooth/palm/TarjanStOrdering.java
+++ src/de/uniwue/smooth/palm/TarjanStOrdering.java
@@ -12,14 +12,21 @@ import java.util.Set;
 import edu.uci.ics.jung.graph.UndirectedGraph;
 
 /**
- * Builds the st-odering for a graph based in Tarjan's algorithm.
- *
+ * Builds the st-Odering for a graph based on Tarjan's algorithm.
+ * [S. Even, R. E. Tarjan: "Computing an st-numbering"; Theoretical Computer Science, 2(3):339–344, 1976.]
+ * 
  * @param <V> Vertex type.
  * @param <E> Edge type.
  */
 public class TarjanStOrdering<V, E> extends AbstractStOrdering<V, E> implements StOrdering<V, E> {
 	
+	/**
+	 * Vertices that are done and won't be visited anymore.
+	 */
 	private Set<V> oldVertices = new HashSet<>();
+	/**
+	 * Edges that are done and won't be traversed anymore.
+	 */
 	private Set<E> oldEdges = new HashSet<>();
 	
 	private UndirectedGraph<V, E> graph;
@@ -27,16 +34,60 @@ public class TarjanStOrdering<V, E> extends AbstractStOrdering<V, E> implements 
 	private V s;
 	private V t;
 	
+	/**
+	 * The vertices end up ordered in this list.
+	 */
 	private List<V> vertexList = new ArrayList<>();
 	
+	/**
+	 * Calculate an st-Ordering
+	 * @param graph Graph whose vertices are ordered.
+	 * @param s Will be the first vertex.
+	 * @param t Will be the last vertex.
+	 */
 	public TarjanStOrdering(UndirectedGraph<V, E> graph, V s, V t) {
 		this.graph = graph;
 		this.palmTree = new PalmTree<V, E>(graph, s, t);
 		this.s = s;
 		this.t = t;
-		initialize();
+		initialize(); // Do the actual calulation
 	}
 	
+	/**
+	 * Pop a vertex from the stack. Then repeatedly
+	 * find paths from new edges between the current
+	 * vertex and an old vertex. Add the path vertices
+	 * to the stack in reverse order. If the edges are
+	 * exhausted, put the vertex in the list and pop the
+	 * next vertex from the stack.
+	 */
+	private void initialize() {
+		oldEdges.add(graph.findEdge(s, t));
+		oldVertices.add(s);
+		oldVertices.add(t);
+		Deque<V> stack = new LinkedList<>();
+		stack.push(t);
+		stack.push(s);
+		while(!stack.isEmpty()) {
+			V v = stack.pop();
+			List<V> path = findPath(v);
+			if(path != null) {
+				for(int i = path.size()-2; i >= 0; i--) {
+					stack.push(path.get(i));
+				}
+			} else {
+				vertexList.add(v);
+			}
+		}
+	}
+	
+	/**
+	 * Find a path between the current vertex and another
+	 * old vertex consisting only of new edges.
+	 *  
+	 * @param start The current vertex to start the path from.
+	 * @return A path from the given vertex to another old vertex, or <tt>null</tt> if the possibilities are exhausted.
+	 */
 	private List<V> findPath(V start) {
 		List<V> path = null;
 		if (path == null) path = findSingleStepBackPath(start);
@@ -45,6 +96,16 @@ public class TarjanStOrdering<V, E> extends AbstractStOrdering<V, E> implements 
 		return path;
 	}
 	
+	/**
+	 * If there is a new (non-old) outgoing cycle edge, it
+	 * is our path. 
+	 * 
+	 * This doesn't change our stack a bit and is basically only
+	 * marking edges as old.
+	 * 
+	 * @param start The current vertex to start the path from.
+	 * @return A path consisting of the current vertex and the endpoint of the non-old back edge, or <tt>null</tt> if there is none.
+	 */
 	private List<V> findSingleStepBackPath(V start) {
 		Collection<E> cycleOutEdges = palmTree.getCycle().getOutEdges(start);
 		for(E e : cycleOutEdges) {
@@ -58,6 +119,14 @@ public class TarjanStOrdering<V, E> extends AbstractStOrdering<V, E> implements 
 		
 	}
 	
+	/**
+	 * If there is a new tree edge exiting our current vertex, we climb the
+	 * tree and follow the route to the lowpointer of the vertex, until
+	 * we hit an old vertex. The route is returned as a path.
+	 * 
+	 * @param start The current vertex to start the path from.
+	 * @return A path consisting of the current vertex and some tree edges, or <tt>null</tt> if there is none.
+	 */
 	private List<V> findClimbingTreePath(V start) {
 		Collection<E> treeEdges = palmTree.getSpanningTrees().getOutEdges(start);
 		for(E e : treeEdges) {
@@ -80,6 +149,13 @@ public class TarjanStOrdering<V, E> extends AbstractStOrdering<V, E> implements 
 		return null;
 	}
 	
+	/**
+	 * If there is an incoming back edge, use it to jump on the tree and traverse the
+	 * tree backwards until we hit something old again.
+	 * 
+	 * @param start The current vertex to start the path from.
+	 * @return A path consisting of the current vertex and a back edge, plus some tree edges, or <tt>null</tt> if there is none.
+	 */
 	private List<V> findJumpOnTreePath(V start) {
 		Collection<E> cycleInEdges = palmTree.getCycle().getInEdges(start);
 		for(E e : cycleInEdges) {
@@ -102,26 +178,6 @@ public class TarjanStOrdering<V, E> extends AbstractStOrdering<V, E> implements 
 		return null;
 	}
 	
-	private void initialize() {
-		oldEdges.add(graph.findEdge(s, t));
-		oldVertices.add(s);
-		oldVertices.add(t);
-		Deque<V> stack = new LinkedList<>();
-		stack.push(t);
-		stack.push(s);
-		while(!stack.isEmpty()) {
-			V v = stack.pop();
-			List<V> path = findPath(v);
-			if(path != null) {
-				for(int i = path.size()-2; i >= 0; i--) {
-					stack.push(path.get(i));
-				}
-			} else {
-				vertexList.add(v);
-			}
-		}
-	}
-
 	@Override
 	public List<V> getList() {
 		return vertexList;
