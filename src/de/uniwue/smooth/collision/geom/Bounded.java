78dcc48527ad8d975ad545cbbdffbafd23345bd0
Bernhard Häussner <bxt@die-optimisten.net>
2014-04-04 02:47:20 +0200

    Add comments to bounded interface

--- src/de/uniwue/smooth/collision/geom/Bounded.java
+++ src/de/uniwue/smooth/collision/geom/Bounded.java
@@ -1,7 +1,14 @@
 package de.uniwue.smooth.collision.geom;
 
+/**
+ * A body/curve that fits into a bounding box.
+ */
 public interface Bounded {
 	
+	/**
+	 * Get the bounding box of this body/curve.
+	 * @return The bounding box containing this body/curve.
+	 */
 	public Box getBoundingBox();
 	
 }
